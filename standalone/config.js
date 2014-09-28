c1dd35adb52e99bdfcd6b5801727e344b2dc93c5
[lowellv@me.com] <lowellv@me.com>
2014-09-27 23:01:17 -0700

    Edit standalone/config.js

--- standalone/config.js
+++ standalone/config.js
@@ -1,10 +1,10 @@
 var config = {
  url : {
-  configuration: 'http://nerd.nu/maps/pve/live/standalone/dynmap_config.json?_={timestamp}',
+  configuration: 'dynmap_config.json?_={timestamp}',
   update: 'http://nerd.nu/maps/pve/live/standalone/dynmap_{world}.json?_={timestamp}',
   sendmessage: 'http://nerd.nu/maps/pve/live/standalone/sendmessage.php',
-  login: 'http://nerd.nu/maps/pve/live/standalone/login.php',
-  register: 'http://nerd.nu/maps/pve/live/standalone/register.php',
+  login: '',
+  register: '',
   tiles: 'http://nerd.nu/maps/pve/live/tiles/',
   markers: 'http://nerd.nu/maps/pve/live/tiles/'
  }
